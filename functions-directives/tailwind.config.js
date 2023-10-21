acf66aec0705b5842902dc34c0bb52b9a2bdc79d
Rajesh Budhathoki <rajoyish@gmail.com>
2023-10-21 22:24:42 +0545

    Demo of Tailwind Functions and directive in action

---
+++ functions-directives/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        128: "32rem",
+      },
+    },
+  },
+  plugins: [],
+};
