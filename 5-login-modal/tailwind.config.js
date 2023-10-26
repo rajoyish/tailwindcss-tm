02b0a17a4d5de2c25b31e2de535d692c8ecde1dd
Rajesh Budhathoki <rajoyish@gmail.com>
2023-10-26 09:52:03 +0545

    Login Modal

---
+++ 5-login-modal/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Mulish", "sans-serif"],
+      mono: ["Rokkitt", "monospace"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
