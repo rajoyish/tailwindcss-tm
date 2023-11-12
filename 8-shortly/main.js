fd3f605cee68a9b6a47fff54d7f9aafc51a85de0
Rajesh Budhathoki <rajoyish@gmail.com>
2023-11-12 12:01:22 +0545

    Shortly Website

---
+++ 8-shortly/main.js
@@ -0,0 +1,45 @@
+import "./style.css";
+
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+
+const input = document.getElementById("link-input");
+const linkForm = document.getElementById("link-form");
+const errMsg = document.getElementById("err-msg");
+
+btn.addEventListener("click", navToggle);
+linkForm.addEventListener("submit", fromSubmit);
+
+// Validate a URL
+function validURL(str) {
+  var pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
+      "((\\d{1,3}\\.){3}\\d{1,3}))" +
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
+      "(\\?[;&a-z\\d%_.~+=-]*)?" +
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  );
+  return !!pattern.test(str);
+}
+
+function fromSubmit(e) {
+  e.preventDefault();
+  if (input.value === "") {
+    errMsg.innerText = "Please enter something";
+    input.classList.add("border-red");
+  } else if (!validURL(input.value)) {
+    errMsg.innerText = "Please enter a valid URL";
+    input.classList.add("border-red");
+  } else {
+    input.classList.remove("border-red");
+    errMsg.innerText = "";
+  }
+}
+
+function navToggle(params) {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+}
